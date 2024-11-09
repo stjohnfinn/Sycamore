caea9d5be3aaa605401580f1bd41e92ec46c7a4d
Finn St John <stjohn.finn@gmail.com>
2024-11-08 22:14:16 -0800

    file complete but not hidden files

--- sycamore-completion.bash
+++ sycamore-completion.bash
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-contains_element() {
-  local e match=$1
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
 _sycamore_completions() {
-  local cur prev opts
+  local cur prev
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
@@ -19,20 +12,33 @@ _sycamore_completions() {
   ###################
 
   for ((i=0; i<COMP_CWORD; i++)); do
-    if [[ "${COMP_WORDS[i]}" == "--file" && $((i+1)) -lt COMP_CWORD ]]; then
-      FILENAME="${COMP_WORDS[i+1]}"
-    fi
+    case "${COMP_WORDS[i]}" in
+      -f|-p|--pipeline-file)
+        if [[ $((i+1)) -lt $COMP_CWORD ]]; then
+          FILENAME="${COMP_WORDS[i+1]}"
+        fi
+        ;;
+    esac
   done
 
   if [[ "$prev" == "--job" || "$prev" == "-j" ]]; then
     [[ -f "$FILENAME" ]] || return 0
     JOB_LIST=$(yq eval ".[] | select(. | has(\"stage\")) | path" -o=tsv "$FILENAME")
+    # shellcheck disable=SC2207
     JOB_ARRAY=($(echo "$JOB_LIST" | tr '\t' ' '))
 
     # shellcheck disable=SC2207
     COMPREPLY=( $(compgen -W "${JOB_ARRAY[*]}" -- "$cur"))
     return 0
   fi
+
+  case "$prev" in
+    -f|-p|--pipeline-file)
+    # shellcheck disable=SC2207
+      COMPREPLY=($(compgen -f -- "$cur" | xargs -I {} bash -c '[[ -f "{}" ]] && echo "{}"'))
+      return 0
+      ;;
+  esac
 }
 
 complete -F _sycamore_completions sycamore.sh
