1a1f221a1386431c719311afdcfe04b28bc7796d
Finn St John <stjohn.finn@gmail.com>
2024-11-23 17:47:14 -0800

    rearrange bigly

--- sycamore.sh
+++ sycamore.sh
@@ -6,13 +6,10 @@ set -o nounset
 set -o pipefail
 set -eu
 
-readonly VERSION="1.1.0"
-GENERATED_SCRIPT="sycamore_gen_$(date +%Y%m%d_%H%M%S).sh"
-readonly GENERATED_SCRIPT
-INTERMEDIATE_FILE=$(mktemp)
-readonly INTERMEDIATE_FILE
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 readonly SCRIPT_NAME
+readonly VERSION="1.1.0"
+GENERATED_SCRIPT="sycamore_gen_$(date +%Y%m%d_%H%M%S).sh"
 
 # Check if yq is installed
 if ! command -v yq &> /dev/null; then
@@ -24,12 +21,16 @@ usage() {
   cat << EOF
 Usage: ${SCRIPT_NAME} pipeline_job_name [ARGUMENTS]
 
-Parse a pipeline job with the specified name. You must provide at least the 
-'--job-name' flag.
+Parses a given pipeline job and creates a file that you can source to 
+automatically export job variables. You must provide at least the '--job-name' 
+flag.
 
 Options:
     -j,--job JOB                Job to be converted to shell script
     -p,-f,--pipeline-file FILE  Path to the pipeline configuration file (default: .gitlab-ci.yml)
+    -s, --show                  Output the contents of the generated file at the end of the script
+    --remove                    Remove the file after converting. Most useful with the "--show" flag.
+    -o, --output                Output the contents to a specific filepath.
     -h,--help                   Show this help message and exit
     -v,--version                Show version information and exit
 EOF
@@ -51,6 +52,8 @@ main() {
   # Initialize variables before processing flags
   local pipeline_file=".gitlab-ci.yml"
   local pipeline_job_name=""
+  local show=false
+  local remove_when_finished=false
 
   # Now process any flags
   while [[ $# -gt 0 ]]; do
@@ -77,6 +80,18 @@ main() {
         pipeline_job_name=$2
         shift 2
         ;;
+      -s|--show)
+        show=true
+        shift
+        ;;
+      --remove)
+        remove_when_finished=true
+        shift
+        ;;
+      -o|--output)
+        GENERATED_SCRIPT=$2
+        shift 2
+        ;;
       *)
         die "Unknown option: $1"
         ;;
@@ -84,7 +99,7 @@ main() {
   done
 
   ##############################################################################
-  # MAIN LOGIC FOR PARSING PIPELINES HERE
+  # Input validation
   ##############################################################################
 
   # Some error handling
@@ -105,33 +120,36 @@ main() {
   fi
 
   # Ok now start creating the shell script
+  if [[ -e "$GENERATED_SCRIPT" ]]; then
+    die "$GENERATED_SCRIPT already exists. exiting..."
+  fi
+  
   touch "$GENERATED_SCRIPT"
-
-  printf "Job: %s (pipeline file: %s)\n" "$pipeline_job_name" "$pipeline_file"
+  
   echo "Creating $GENERATED_SCRIPT."
 
-  # shebang
-  echo -e "#!/usr/bin/env bash\n" >> "$GENERATED_SCRIPT"
+  if ! $remove_when_finished; then
+    # shebang
+    echo -e "#!/usr/bin/env bash" >> "$GENERATED_SCRIPT"
 
-  # options so that if they `source` the file with unset variables it fails
-  # INTENDED & TESTED WITH BASH ONLY
-  echo -e "set -u\n" >> "$GENERATED_SCRIPT"
-
-  ###################
-  # Global variables
-  ###################
-  if yq -e ".variables" "$pipeline_file" &> /dev/null; then
-    printf "# Global variables\n" >> "$GENERATED_SCRIPT"
-    yq eval -o=json ".variables" "$pipeline_file" | jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' >> "$GENERATED_SCRIPT"
-    echo "" >> "$GENERATED_SCRIPT"
+    # options so that if they `source` the file with unset variables it fails
+    # INTENDED & TESTED WITH BASH ONLY
+    echo -e "set -u\n" >> "$GENERATED_SCRIPT"
   fi
 
-  ###################
+  ##############################################################################
+  # Global variables
+  ##############################################################################
+  if yq -e ".variables" "$pipeline_file" &> /dev/null; then
+    # shellcheck disable=SC2129
+    yq eval -o=json ".variables" "$pipeline_file" | jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' >> "$GENERATED_SCRIPT"
+  fi
+
+  ##############################################################################
   # Job variables
-  ###################
+  ##############################################################################
 
   if yq -e ".\"$pipeline_job_name\".variables" "$pipeline_file" &> /dev/null; then
-    printf "# Job variables\n" >> "$GENERATED_SCRIPT"
     
     if yq -e ".\"$pipeline_job_name\".extends" "$pipeline_file" &> /dev/null; then
       extends_list=$(yq -e -o=tsv ".\"$pipeline_job_name\".extends" "$pipeline_file")
@@ -145,9 +163,15 @@ main() {
       done
     fi
 
-
     yq eval -o=json ".\"$pipeline_job_name\".variables" "$pipeline_file" | jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' >> "$GENERATED_SCRIPT"
-    echo "" >> "$GENERATED_SCRIPT"
+  fi
+
+  if $show; then
+    cat "$GENERATED_SCRIPT"
+  fi
+
+  if $remove_when_finished; then
+    rm -f "$GENERATED_SCRIPT"
   fi
 }
 
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Nothing here yet..."
+test() {
+  local input_file=$1
+  
+}
+
+pushd test || exit 1
+
+CMD=../sycamore.sh
+
+$CMD --pipeline-file .gitlab-ci.yml --job build.job
+
+
